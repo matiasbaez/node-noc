0651266846ce8be242dc9f4e1be62843ace53fd0
Matias Baez <matiasbaez2512@gmail.com>
2024-03-23 20:50:03 -0300

    Improve log entity to log origin

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -5,26 +5,46 @@ export enum LogSeverityLevel {
     HIGH = "HIGH"
 }
 
+export interface LogEntityOptions {
+    level: LogSeverityLevel;
+    message: string;
+    createdAt?: Date;
+    origin: string;
+}
+
 export class LogEntity {
 
     public level: LogSeverityLevel;
     public message: string;
     public createdAt: Date;
+    public origin: string;
 
-    constructor(message: string, level: LogSeverityLevel) {
+    // constructor(message: string, level: LogSeverityLevel) {
+    //     this.level = level;
+    //     this.message = message;
+    //     this.createdAt = new Date();
+    // }
+
+    constructor(options: LogEntityOptions) {
+        const { level, message, origin, createdAt = new Date() } = options;
         this.level = level;
+        this.origin = origin;
         this.message = message;
-        this.createdAt = new Date();
+        this.createdAt = createdAt;
     }
 
     static fromJSON(jsonObject: string): LogEntity {
-        const { message, level, createdAt } = JSON.parse(jsonObject);
+        const { message, level, createdAt, origin } = JSON.parse(jsonObject);
 
         if (!message) throw new Error("message is required");
         if (!level) throw new Error("level is required");
 
-        const log = new LogEntity(message, level);
-        log.createdAt =  createdAt ? new Date(createdAt) : log.createdAt;
+        const log = new LogEntity({
+            message,
+            level,
+            createdAt,
+            origin
+        });
 
         return log;
     }
